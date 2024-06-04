ccafda6b504d0af340904bc5af00d9fae72f31a2
MdSikder <mdporagbsmrstu@gmail.com>
2024-06-04 16:51:37 +0600

    use config.ini files info- base_url

--- tests/integration_tests/test_api_endpoints.py
+++ tests/integration_tests/test_api_endpoints.py
@@ -1,18 +1,22 @@
 import logging
 import requests
 import pytest
+from src.config_parser import ReadConfig
 
 # Get the logger
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
-# body for authentication
+# Body for authentication
 body = {
     "username": "admin",
     "password": "password123"
 }
 
 
-def get_auth_token(base_url):
+def get_auth_token():
+    base_url = ReadConfig.get_base_url()
+    logger.info("Entering get_auth_token")
     # Endpoint for creating a token
     auth_url = f"{base_url}/auth"
 
@@ -28,9 +32,9 @@ def get_auth_token(base_url):
     return data['token']
 
 
-def test_get_auth_token(base_url):
+def test_get_auth_token():
     # Get the token using the function
-    token = get_auth_token(base_url)
+    token = get_auth_token()
 
     # Print the token
     logger.info(f"Token: {token}")
